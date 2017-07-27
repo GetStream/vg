c46a060484ce1218250e75cd1499712c8955ec4c
Jelte Fennema <github-tech@jeltef.nl>
2017-07-27 15:26:51 +0200

    localInstall: Uninstall compiled versions when runnning localInstall

--- cmd/uninstall.go
+++ cmd/uninstall.go
@@ -6,9 +6,7 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 
-	"github.com/GetStream/vg/internal/utils"
 	"github.com/GetStream/vg/internal/workspace"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
@@ -34,57 +32,8 @@ var uninstallCmd = &cobra.Command{
 		}
 
 		for _, pkg := range args {
-			// pkgComponents := strings.Split(pkg, hello
-			pkgDir := utils.PkgToDir(pkg)
 			fmt.Printf("Uninstalling %q from workspace\n", pkg)
-			err := os.RemoveAll(filepath.Join(ws.Src(), pkgDir))
-			if err != nil {
-				return errors.Wrapf(err, "Couldn't remove package src '%s'", ws.Name())
-			}
-
-			pkgInstalledDirs, err := filepath.Glob(filepath.Join(ws.Pkg(), "*", pkgDir))
-			if err != nil {
-				return errors.Wrapf(err, "Something went wrong when globbing files for '%s'", pkg)
-			}
-
-			for _, path := range pkgInstalledDirs {
-				fmt.Println("Removing", path)
-
-				err = os.RemoveAll(path)
-				if err != nil {
-					return errors.Wrapf(err, "Couldn't remove installed package files for '%s'", pkg)
-				}
-			}
-
-			pkgInstalledFiles, err := filepath.Glob(filepath.Join(ws.Pkg(), "*", pkgDir+".a"))
-			if err != nil {
-				return errors.Wrapf(err, "Something went wrong when globbing files for '%s'", pkg)
-			}
-
-			for _, path := range pkgInstalledFiles {
-				fmt.Println("Removing", path)
-
-				err = os.RemoveAll(path)
-				if err != nil {
-					return errors.Wrapf(err, "Couldn't remove installed package files for '%s'", pkg)
-				}
-			}
-
-			settings, err := ws.Settings()
-			if err != nil {
-				return err
-			}
-
-			if _, ok := settings.LocalInstalls[pkg]; ok {
-				fmt.Printf("Removing %q from persistent local installs\n", pkg)
-				delete(settings.LocalInstalls, pkg)
-
-				err = ws.SaveSettings(settings)
-				if err != nil {
-					return err
-				}
-			}
-
+			ws.Uninstall(pkg, os.Stdout)
 		}
 		return nil
 	},
--- internal/workspace/localInstall.go
+++ internal/workspace/localInstall.go
@@ -2,19 +2,26 @@ package workspace
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path"
 	"path/filepath"
 
+	"github.com/GetStream/vg/internal/utils"
 	"github.com/pkg/errors"
 )
 
 func (ws *Workspace) InstallLocalPackage(pkg string, localPath string) error {
+	err := ws.Uninstall(pkg, os.Stderr)
+	if err != nil {
+		return err
+	}
+
 	_, _ = fmt.Fprintf(os.Stderr, "Installing local sources at %q in workspace as %q\n", localPath, pkg)
 	pkgDir := filepath.Join(path.Split(pkg))
 	linkName := filepath.Join(ws.Src(), pkgDir)
 
-	err := os.MkdirAll(filepath.Dir(linkName), 0755)
+	err = os.MkdirAll(filepath.Dir(linkName), 0755)
 	if err != nil {
 		return errors.WithStack(err)
 	}
@@ -39,5 +46,58 @@ func (ws *Workspace) InstallPersistentLocalPackages() error {
 			return err
 		}
 	}
+	return nil
+}
+
+func (ws *Workspace) Uninstall(pkg string, logWriter io.Writer) error {
+	pkgDir := utils.PkgToDir(pkg)
+	err := os.RemoveAll(filepath.Join(ws.Src(), pkgDir))
+	if err != nil {
+		return errors.Wrapf(err, "Couldn't remove package src '%s'", ws.Name())
+	}
+
+	pkgInstalledDirs, err := filepath.Glob(filepath.Join(ws.Pkg(), "*", pkgDir))
+	if err != nil {
+		return errors.Wrapf(err, "Something went wrong when globbing files for '%s'", pkg)
+	}
+
+	for _, path := range pkgInstalledDirs {
+		fmt.Fprintln(logWriter, "Removing", path)
+
+		err = os.RemoveAll(path)
+		if err != nil {
+			return errors.Wrapf(err, "Couldn't remove installed package files for '%s'", pkg)
+		}
+	}
+
+	pkgInstalledFiles, err := filepath.Glob(filepath.Join(ws.Pkg(), "*", pkgDir+".a"))
+	if err != nil {
+		return errors.Wrapf(err, "Something went wrong when globbing files for '%s'", pkg)
+	}
+
+	for _, path := range pkgInstalledFiles {
+		fmt.Fprintln(logWriter, "Removing", path)
+
+		err = os.RemoveAll(path)
+		if err != nil {
+			return errors.Wrapf(err, "Couldn't remove installed package files for '%s'", pkg)
+		}
+	}
+
+	settings, err := ws.Settings()
+	if err != nil {
+		return err
+	}
+
+	if _, ok := settings.LocalInstalls[pkg]; ok {
+		fmt.Fprintf(logWriter, "Removing %q from persistent local installs\n", pkg)
+		delete(settings.LocalInstalls, pkg)
+
+		err = ws.SaveSettings(settings)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
