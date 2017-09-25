935c8ad6ac7e301ccdad314d84140fda2c1b1401
David Glasser <glasser@davidglasser.net>
2017-09-25 11:58:24 -0700

    cmd: comment out unused field from cobra template

--- cmd/root.go
+++ cmd/root.go
@@ -61,5 +61,5 @@ func Execute() {
 func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
